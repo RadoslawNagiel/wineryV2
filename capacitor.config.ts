74dde4b9848acdc814bab156d283b987701c0313
Radek <radoslaw.nagiel1@gmail.com>
2023-07-23 17:42:41 +0200

    Init Project

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'pl.rn.winery',
+  appName: 'Winery',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
