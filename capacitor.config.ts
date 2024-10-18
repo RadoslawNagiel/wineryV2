a2695df4379301f874df0b90ebcbd718a5dd2347
RadoslawNagiel <nagiel.media@gmail.com>
2024-10-18 15:30:12 +0200

    update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
     appId: `com.bekarn.winery`,
-    appName: `winery`,
+    appName: `Winery`,
     webDir: `www`,
+    plugins: {
+        SplashScreen: {
+            androidScaleType: `CENTER_CROP`,
+            launchAutoHide: true,
+            splashFullScreen: false,
+            splashImmersive: false,
+        },
+    },
     // server: {
     //     cleartext: true,
     //     url: `http://192.168.1.58:4200`,
